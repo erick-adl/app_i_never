92ed150a0d274608b77aca892724c03db67b55ff
erick-adl <erick-adlima@gmail.com>
2022-01-18 08:49:57 -0300

    addd cupertino page route and new transition page

--- lib/src/core/route/route.dart
+++ lib/src/core/route/route.dart
@@ -1,5 +1,6 @@
 import 'package:app_i_never/src/feature/presentation/ui/home.page.dart';
 import 'package:app_i_never/src/feature/presentation/ui/suggest.page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
@@ -16,7 +17,7 @@ class AppRoutes {
       case homePage:
         return MaterialPageRoute(builder: (context) => HomePage());
       case suggestPage:
-        return MaterialPageRoute(builder: (context) => SuggestPage());
+        return CupertinoPageRoute(builder: (context) => SuggestPage());
       case gamePage:
         return MaterialPageRoute(builder: (context) => Container());
       default:
